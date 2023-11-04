f5e48084ab27d030eda261ec090dddaac516b6ab
Pradeep87 <pradeep87055@gmail.com>
2023-11-04 10:54:13 +0530

    namaste react episode first

---
+++ app.js
@@ -0,0 +1,15 @@
+const parent = React.createElement("div", {}, [
+  React.createElement("div", { id: "childOne" }, [
+    React.createElement("h1", { id: "heading one" }, "heading one"),
+    React.createElement("h1", { id: "heading two" }, "heading Two"),
+  ]),
+  React.createElement("div", { id: "childTwo" }, [
+    React.createElement("h1", { id: "heading one" }, "heading one in child two"),
+    React.createElement("h1", { id: "heading two" }, "heading Two in child two"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(parent);
+root.render(parent);
