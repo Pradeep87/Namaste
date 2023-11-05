0b3ed193314e9f7ea37d1373d004f234f226e45e
Pradeep87 <pradeep87055@gmail.com>
2023-11-05 13:04:30 +0530

    app coverted

--- src/app.js
+++ src/app.js
@@ -1,19 +1,11 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
 
+const App = () => {
+  return (
+    <div>
+      <h1>hello from the app</h1>
+    </div>
+  );
+};
 
-const parent = React.createElement("div", {}, [
-  React.createElement("div", { id: "childOne" }, [
-    React.createElement("h1", { id: "heading one" }, "heading one"),
-    React.createElement("h1", { id: "heading two" }, "heading Two"),
-  ]),
-  React.createElement("div", { id: "childTwo" }, [
-    React.createElement("h1", { id: "heading one" }, "heading one in child two"),
-    React.createElement("h1", { id: "heading two" }, "heading Two in child two"),
-  ]),
-]);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-console.log(parent);
-root.render(parent);
+export default App;
